fd350eb926d3d2a280a62cf725c9b0bd66332557
00komiya <114364060+00komiya@users.noreply.github.com>
2022-10-13 13:23:31 +0000

    [add]kadai2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-  resources :books
+  root to: 'homes#top'
+  
+  resources :books, only:   [:index, :show, :edit, :create, :destroy, :update] do
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
